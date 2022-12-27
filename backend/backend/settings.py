e481d834948a4a1a19d6d083cde79969f1d906f4
Cameron2310 <wilke7566@gmail.com>
2022-12-26 16:47:25 -1000

    changed settings for deployment

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -136,6 +136,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS__ORIGIN_ALLOW_ALL = True
 
 AUTH_USER_MODEL = 'website.User'
+
+CSRF_TRUSTED_ORIGINS = ['astronomywebsite.net']
 
 
 REST_FRAMEWORK = {
