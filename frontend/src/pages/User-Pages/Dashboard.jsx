924c57e4239398ab3cb1a66d5702d281baa9d728
Cameron2310 <wilke7566@gmail.com>
2022-12-30 16:21:46 -1000

    debugged API call problems on frontend

--- frontend/src/pages/User-Pages/Dashboard.jsx
+++ frontend/src/pages/User-Pages/Dashboard.jsx
@@ -28,15 +28,34 @@ export default function Dashboard() {
   };
 
   const saveData = async () => {
-    const response = await axios.put("http://localhost:8000/userdata/", {
-      params: {
-        user_id: decryptedUserId,
-        username: username.current.value,
-        first_name: firstName.current.value,
-        last_name: lastName.current.value,
-        favorite_planet: favoritePlanet.current.value,
-      },
-    });
+    const response = await axios
+      .put("/userdata/", {
+        params: {
+          user_id: decryptedUserId,
+          username: username.current.value,
+          first_name: firstName.current.value,
+          last_name: lastName.current.value,
+          favorite_planet: favoritePlanet.current.value,
+        },
+      })
+      .catch(function (error) {
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        } else if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+          // http.ClientRequest in node.js
+          console.log(error.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log("Error", error.message);
+        }
+        console.log(error.config);
+      });
     setUserInformation(response.data[0]);
     clearInputs();
   };
@@ -53,11 +72,30 @@ export default function Dashboard() {
 
   useEffect(() => {
     const getUserInformation = async () => {
-      const response = await axios("http://localhost:8000/userdata/", {
-        params: {
-          user_id: decryptedUserId,
-        },
-      });
+      const response = await axios
+        .get("/userdata/", {
+          params: {
+            user_id: decryptedUserId,
+          },
+        })
+        .catch(function (error) {
+          if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+          } else if (error.request) {
+            // The request was made but no response was received
+            // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+            // http.ClientRequest in node.js
+            console.log(error.request);
+          } else {
+            // Something happened in setting up the request that triggered an Error
+            console.log("Error", error.message);
+          }
+          console.log(error.config);
+        });
       setUserInformation(response.data);
     };
     getUserInformation();
