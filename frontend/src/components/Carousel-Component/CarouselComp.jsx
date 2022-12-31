c54bf07a7e1c68d0487ae60b8170e27b7427c02b
Cameron2310 <wilke7566@gmail.com>
2022-12-30 15:59:39 -1000

    changes made

--- frontend/src/components/Carousel-Component/CarouselComp.jsx
+++ frontend/src/components/Carousel-Component/CarouselComp.jsx
@@ -10,26 +10,24 @@ export default function CarouselComp() {
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await axios
-        .get("http://localhost:8000/images/")
-        .catch(function (error) {
-          if (error.response) {
-            // The request was made and the server responded with a status code
-            // that falls out of the range of 2xx
-            console.log(error.response.data);
-            console.log(error.response.status);
-            console.log(error.response.headers);
-          } else if (error.request) {
-            // The request was made but no response was received
-            // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-            // http.ClientRequest in node.js
-            console.log(error.request);
-          } else {
-            // Something happened in setting up the request that triggered an Error
-            console.log("Error", error.message);
-          }
-          console.log(error.config);
-        });
+      const response = await axios.get("images/").catch(function (error) {
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        } else if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+          // http.ClientRequest in node.js
+          console.log(error.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log("Error", error.message);
+        }
+        console.log(error.config);
+      });
       console.log("running ...");
       console.log(response.status);
       setImages(response.data);
