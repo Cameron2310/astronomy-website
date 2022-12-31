7a542ccc6532fffd40c4f264d084bf45888f17f3
Cameron2310 <wilke7566@gmail.com>
2022-12-31 12:01:46 -1000

    changed settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
