4785e2195a389fa08184681021686460d6713b64
Cameron2310 <wilke7566@gmail.com>
2022-12-30 16:11:47 -1000

    changes made

--- frontend/src/pages/Community.jsx
+++ frontend/src/pages/Community.jsx
@@ -47,7 +47,24 @@ export default function Community() {
 
   useEffect(() => {
     const getPosts = async () => {
-      const response = await axios.get("posts/");
+      const response = await axios.get("posts/").catch(function (error) {
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        } else if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+          // http.ClientRequest in node.js
+          console.log(error.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log("Error", error.message);
+        }
+        console.log(error.config);
+      });
       setPosts(response.data);
     };
     getPosts();
--- frontend/src/pages/HomePages/PhotoPage.jsx
+++ frontend/src/pages/HomePages/PhotoPage.jsx
@@ -8,8 +8,25 @@ export default function PhotoPage() {
 
   useEffect(() => {
     const fetchPhoto = async () => {
-      const response = await axios("filter_images/", {
+      const response = await axios("/filter_images/", {
         params: { photo_name: photoName },
+      }).catch(function (error) {
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        } else if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+          // http.ClientRequest in node.js
+          console.log(error.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log("Error", error.message);
+        }
+        console.log(error.config);
       });
       setPhotoData(response.data);
       console.log(response.data);
