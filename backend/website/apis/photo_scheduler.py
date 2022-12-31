047b635e27b5f3ca3d131a2b911d4e604948e9d2
Cameron2310 <wilke7566@gmail.com>
2022-12-31 12:51:15 -1000

    debugging APscheduler

--- backend/website/apis/photo_scheduler.py
+++ backend/website/apis/photo_scheduler.py
@@ -1,9 +1,8 @@
 from apscheduler.schedulers.background import BackgroundScheduler
-import daily_photo_API as api
+import website.apis.daily_photo_API as api
 
 
 def start():
-    print("Scheduler called ...")
     scheduler = BackgroundScheduler()
     scheduler.add_job(api.save_image, "interval", minutes=2,
                       id="photo_001", replace_existing=True)
