f6b51ef4a8f8fdef108e5f0bba292320fdce392e
Cameron2310 <wilke7566@gmail.com>
2022-12-31 14:03:08 -1000

    shell command added

--- backend/website/apis/run_photo_scheduler.sh
+++ backend/website/apis/run_photo_scheduler.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-python3 /Users/cameron/Desktop/CodePlatoon/assessments/astronomy-website/backend/website/apis/photo_scheduler.py
+python3 /~/Desktop/CodePlatoon/assessments/astronomy-website/backend/website/apis/photo_scheduler.py
